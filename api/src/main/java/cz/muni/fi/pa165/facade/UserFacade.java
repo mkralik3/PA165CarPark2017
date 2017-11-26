b4bba3219cf1bf3cf688d32bb10eb98f83d444f1
Martin Miškeje <manager.programmer@gmail.com>
2017-11-26 11:36:39 +0100

    Dozer - support for enums conversion, RegionalBranchFacade create implementation

--- api/src/main/java/cz/muni/fi/pa165/facade/UserFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/facade/UserFacade.java
@@ -8,8 +8,6 @@ import java.util.List;
 @author Martin Miškeje
 */
 public interface UserFacade {
-
-    ResultWithData<UserDTO> authenticate(AuthenticationDataDTO authData);
     
     UserOperationResult registerUser(UserDTO user, String password);
 
--- service/src/main/java/cz/muni/fi/pa165/facade/RegionalBranchFacadeImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/facade/RegionalBranchFacadeImpl.java
@@ -2,13 +2,33 @@ package cz.muni.fi.pa165.facade;
 
 import cz.muni.fi.pa165.dto.*;
 import cz.muni.fi.pa165.dto.results.*;
+import cz.muni.fi.pa165.entity.RegionalBranch;
+import cz.muni.fi.pa165.service.*;
 import java.util.List;
+import javax.inject.Inject;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
 public class RegionalBranchFacadeImpl implements RegionalBranchFacade {
 
+    @Inject
+    private RegionalBranchService branchService;
+    @Inject
+    private BeanMappingService beanMappingService;
     @Override
     public RegionalBranchOperationResult createRegionalBranch(RegionalBranchDTO branch) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        RegionalBranchOperationResult result = new RegionalBranchOperationResult();
+        try {
+            RegionalBranch branchToCreate = beanMappingService.mapTo(branch, RegionalBranch.class);
+            branchService.create(branchToCreate);
+            result.setData(beanMappingService.mapTo(branchToCreate, RegionalBranchDTO.class));
+            result.setIsSuccess(true);
+        } catch (Exception ex) {
+            // todo log
+        }
+        return result;
     }
 
     @Override
