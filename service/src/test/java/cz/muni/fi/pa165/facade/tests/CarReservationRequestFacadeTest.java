53287056e3d1c7bbf6f3135be1f6f15289ba42cf
Denis Richtarik <denis.richtarik@gmail.com>
2017-11-27 23:01:07 +0100

    Add tests for CarReservationRequest facade, service and dao

---
+++ service/src/test/java/cz/muni/fi/pa165/facade/tests/CarReservationRequestFacadeTest.java
@@ -0,0 +1,169 @@
+package cz.muni.fi.pa165.facade.tests;
+
+import cz.muni.fi.pa165.dto.CarDTO;
+import cz.muni.fi.pa165.dto.CarReservationRequestDTO;
+import cz.muni.fi.pa165.dto.RegionalBranchDTO;
+import cz.muni.fi.pa165.dto.UserDTO;
+import cz.muni.fi.pa165.entity.Car;
+import cz.muni.fi.pa165.entity.CarReservationRequest;
+import cz.muni.fi.pa165.entity.User;
+import cz.muni.fi.pa165.enums.CarReservationRequestState;
+import cz.muni.fi.pa165.enums.UserType;
+import cz.muni.fi.pa165.facade.CarReservationRequestFacade;
+import cz.muni.fi.pa165.service.CarReservationRequestService;
+import cz.muni.fi.pa165.tests.support.TestObjectFactory;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+public class CarReservationRequestFacadeTest extends BaseFacadeTest {
+    @Mock
+    private CarReservationRequestService carReservationRequestService;
+
+    @Mock
+    private CarDTO carDTO;
+
+    @Mock
+    private UserDTO userDTO;
+
+    @Mock
+    private RegionalBranchDTO regionalBranchDTO;
+
+    @Autowired
+    @InjectMocks
+    private CarReservationRequestFacade carReservationRequestFacade;
+
+    private CarReservationRequestDTO reservationRequestDTO;
+
+    private LocalDateTime currentTime;
+
+    private final TestObjectFactory objectFactory = new TestObjectFactory();
+
+
+    private Car car1;
+    private User user1;
+    private CarReservationRequest reservation1;
+
+    @BeforeMethod()
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        carReservationRequestFacade = (CarReservationRequestFacade) unwrapProxy(carReservationRequestFacade);
+        ReflectionTestUtils.setField(carReservationRequestFacade, "reservationsService", carReservationRequestService);
+        ReflectionTestUtils.setField(carReservationRequestFacade, "beanMappingService", beanMappingService);
+
+        currentTime = LocalDateTime.now();
+
+        user1 = objectFactory.createUser("firstUser", UserType.USER);
+        user1.setId(Long.valueOf(1));
+
+        userDTO = new UserDTO();
+        userDTO.setUserName("firstUser");
+        userDTO.setType(cz.muni.fi.pa165.dto.enums.UserType.USER);
+        userDTO.setId(Long.valueOf(1));
+
+        car1 = objectFactory.createCar("sampleCar");
+        car1.setId(Long.valueOf(1));
+
+        carDTO = new CarDTO();
+        carDTO.setName("sampleCar");
+        carDTO.setId(Long.valueOf(1));
+
+        reservation1 = new CarReservationRequest();
+        reservation1.setId(1L);
+        reservation1.setCar(car1);
+        reservation1.setUser(user1);
+        reservation1.setReservationStartDate(currentTime);
+        reservation1.setReservationEndDate(currentTime.plus(4, ChronoUnit.DAYS));
+        reservation1.setState(CarReservationRequestState.CREATED);
+
+        reservationRequestDTO = new CarReservationRequestDTO();
+        reservationRequestDTO.setId(1L);
+        reservationRequestDTO.setCar(carDTO);
+        reservationRequestDTO.setUser(userDTO);
+        reservationRequestDTO.setReservationStartDate(currentTime);
+        reservationRequestDTO.setReservationEndDate(currentTime.plus(4, ChronoUnit.DAYS));
+        reservationRequestDTO.setState(cz.muni.fi.pa165.dto.enums.CarReservationRequestState.CREATED);
+
+    }
+
+    @Test
+    public void createCarReservationRequestTest() {
+        when(beanMappingService.mapTo(reservationRequestDTO, CarReservationRequest.class)).thenReturn(reservation1);
+        carReservationRequestFacade.createCarReservationRequest(reservationRequestDTO);
+
+        verify(carReservationRequestService).create(reservation1);
+        verify(beanMappingService).mapTo(reservationRequestDTO, CarReservationRequest.class);
+    }
+
+    @Test
+    public void testUpdateCar() {
+        CarReservationRequestDTO updatedDTO = reservationRequestDTO;
+        updatedDTO.setReservationEndDate(currentTime.plus(5, ChronoUnit.DAYS));
+        CarReservationRequest updatedEntity = reservation1;
+        reservation1.setReservationEndDate(currentTime.plus(5, ChronoUnit.DAYS));
+
+        when(beanMappingService.mapTo(reservationRequestDTO, CarReservationRequest.class)).thenReturn(updatedEntity);
+
+        carReservationRequestFacade.updateCarReservationRequest(updatedDTO);
+
+        verify(carReservationRequestService).update(updatedEntity);
+        verify(beanMappingService).mapTo(reservationRequestDTO, CarReservationRequest.class);
+    }
+
+
+    @Test
+    public void testDeleteCar() {
+        carReservationRequestFacade.deleteCarReservationRequest(reservation1.getId());
+        verify(carReservationRequestService).delete(reservation1.getId());
+    }
+
+    @Test
+    public void getAllForRegionalBranchTest() {
+        LocalDateTime today = LocalDateTime.now();
+
+        Set<Long> requiredBranchIds = new HashSet<>();
+        requiredBranchIds.add(1L);
+
+        CarReservationRequest crr = new CarReservationRequest();
+        crr.setCar(car1);
+        crr.setUser(user1);
+        crr.setState(CarReservationRequestState.APPROVED);
+        crr.setReservationStartDate(today);
+        crr.setReservationStartDate(today.plus(5, ChronoUnit.DAYS));
+
+        CarReservationRequestDTO crrDTO = new CarReservationRequestDTO();
+        crrDTO.setCar(carDTO);
+        crrDTO.setUser(userDTO);
+        crr.setState(CarReservationRequestState.APPROVED);
+        crr.setReservationStartDate(today);
+        crr.setReservationStartDate(today.plus(5, ChronoUnit.DAYS));
+
+        when(carReservationRequestService.getAllForRegionalBranch(requiredBranchIds, today, today.plus(5, ChronoUnit.DAYS)))
+                .thenReturn(Arrays.asList(crr));
+
+        when(beanMappingService.mapTo(crr, CarReservationRequestDTO.class)).thenReturn(crrDTO);
+
+        List<CarReservationRequestDTO> result = carReservationRequestFacade.getAllForRegionalBranch(requiredBranchIds, today, today.plus(5, ChronoUnit.DAYS));
+
+        verify(carReservationRequestService).getAllForRegionalBranch(requiredBranchIds, today, today.plus(5, ChronoUnit.DAYS));
+        verify(beanMappingService).mapTo(crr, CarReservationRequestDTO.class);
+
+        assertThat(result).contains(crrDTO);
+    }
+}
---
+++ service/src/test/java/cz/muni/fi/pa165/service/tests/CarReservationRequestServiceTest.java
@@ -0,0 +1,192 @@
+package cz.muni.fi.pa165.service.tests;
+
+import cz.muni.fi.pa165.dao.CarDAO;
+import cz.muni.fi.pa165.dao.CarReservationRequestDAO;
+import cz.muni.fi.pa165.dao.RegionalBranchDAO;
+import cz.muni.fi.pa165.dao.UserDAO;
+import cz.muni.fi.pa165.entity.Car;
+import cz.muni.fi.pa165.entity.CarReservationRequest;
+import cz.muni.fi.pa165.entity.RegionalBranch;
+import cz.muni.fi.pa165.entity.User;
+import cz.muni.fi.pa165.enums.CarReservationRequestState;
+import cz.muni.fi.pa165.enums.UserType;
+import cz.muni.fi.pa165.service.CarReservationRequestService;
+import cz.muni.fi.pa165.service.CarReservationRequestServiceImpl;
+import cz.muni.fi.pa165.service.TimeService;
+import cz.muni.fi.pa165.tests.support.TestObjectFactory;
+import org.hibernate.service.spi.ServiceException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+public class CarReservationRequestServiceTest extends BaseServiceTest {
+
+    @Mock
+    private CarReservationRequestDAO carReservationRequestDAO;
+
+    @Mock
+    private UserDAO userDao;
+
+    @Mock
+    private CarDAO carDao;
+
+    @Mock
+    private TimeService timeService;
+
+    @Mock
+    private RegionalBranchDAO regionalBranchDao;
+
+    @InjectMocks
+    private CarReservationRequestService carReservationRequestService = new CarReservationRequestServiceImpl();
+
+    private final TestObjectFactory objectFactory = new TestObjectFactory();
+
+    private LocalDateTime currentTime;
+    private Car car1;
+    private Car car2;
+    private User user1;
+    private User user2;
+    private CarReservationRequest reservation1;
+    private CarReservationRequest reservation2;
+
+    @BeforeMethod
+    public void setup() throws ServiceException {
+
+        currentTime = LocalDateTime.now();
+
+        user1 = objectFactory.createUser("firstUser", UserType.USER);
+        user1.setId(Long.valueOf(1));
+        user2 = objectFactory.createUser("secondUser", UserType.USER);
+        user2.setId(Long.valueOf(2));
+
+        userDao.save(user1);
+        userDao.save(user2);
+
+        car1 = objectFactory.createCar("sampleCar");
+        car1.setId(Long.valueOf(1));
+        car2 = objectFactory.createCar("sampleCar2");
+        car2.setId(Long.valueOf(2));
+
+        carDao.save(car1);
+        carDao.save(car2);
+
+        reservation1 = objectFactory.createReservationRequest(car1, user1, currentTime, currentTime.plus(4, ChronoUnit.DAYS), CarReservationRequestState.CREATED);
+        reservation1.setId(Long.valueOf(1));
+        reservation2 = objectFactory.createReservationRequest(car2, user2, currentTime, currentTime.plus(2, ChronoUnit.DAYS), CarReservationRequestState.CREATED);
+
+        when(carReservationRequestDAO.findOne(Long.valueOf(1)))
+                .thenReturn(reservation1);
+
+        when(carReservationRequestDAO.findAll())
+                .thenReturn(Arrays.asList(reservation1, reservation2));
+        when(userDao.findOne(1L))
+                .thenReturn(user1);
+        when(userDao.findOne(2L))
+                .thenReturn(user2);
+
+        when(carDao.findOne(1L))
+                .thenReturn(car1);
+        when(carDao.findOne(2L))
+                .thenReturn(car2);
+
+        doAnswer((Answer<Object>) (InvocationOnMock invocation) -> {
+            Object argument = invocation.getArguments()[0];
+            if (argument == null) {
+                throw new IllegalArgumentException();
+            }
+
+            CarReservationRequest carReservationRequest = (CarReservationRequest) argument;
+            if (carReservationRequest.getId() != null) {
+                throw new IllegalArgumentException();
+            }
+
+            carReservationRequest.setId(Long.valueOf(1));
+            return null;
+        }).when(carReservationRequestDAO).save(any(CarReservationRequest.class));
+
+        doAnswer((Answer<Void>) (InvocationOnMock invocation) -> {
+            Object argument = invocation.getArguments()[0];
+
+            long userId = (long) argument;
+            if (userId < 0) {
+                throw new IllegalArgumentException();
+            }
+
+            return null;
+        }).when(carReservationRequestDAO).delete(any(long.class));
+    }
+
+    @BeforeClass
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+
+    @Test
+    public void createReservation() throws IllegalArgumentException {
+        carReservationRequestService.create(reservation2);
+        assertThat(reservation2.getId()).isGreaterThan(0);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void createNullReservation() throws IllegalArgumentException {
+        carReservationRequestService.create(null);
+    }
+
+    @Test
+    public void updateReservation() throws IllegalArgumentException {
+        reservation1.setCar(car2);
+        carReservationRequestService.update(reservation1);
+        assertThat(reservation1.getCar()).isEqualTo(car2);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void updateNullReservation() throws IllegalArgumentException {
+        carReservationRequestService.update(null);
+    }
+
+    @Test
+    public void deleteNonExistingReservation() throws IllegalArgumentException {
+        CarReservationRequest deletedReservationRequest = carReservationRequestService.delete(-1L);
+        assertThat(deletedReservationRequest).isNull();
+    }
+
+    @Test
+    public void deleteReservation() throws IllegalArgumentException {
+        CarReservationRequest deletedReservation = carReservationRequestService.delete(reservation1.getId());
+        Mockito.verify(carReservationRequestDAO, Mockito.times(1)).delete(reservation1);
+        assertThat(deletedReservation).isEqualToComparingFieldByField(reservation1);
+    }
+
+    @Test
+    public void getAllForRegionalBranchTest() {
+        RegionalBranch regionalBranch = objectFactory.createRegionalBranch("testBranch");
+        regionalBranch.addCar(car1);
+        regionalBranchDao.save(regionalBranch);
+
+        Set<Long> requiredBranchIds = new HashSet<>();
+        requiredBranchIds.add(reservation1.getId());
+
+        List<CarReservationRequest> foundReservations = carReservationRequestDAO.getAllForRegionalBranch(requiredBranchIds, currentTime, currentTime.plus(4, ChronoUnit.DAYS));
+
+        assertThat(foundReservations.contains(reservation1));
+    }
+}
