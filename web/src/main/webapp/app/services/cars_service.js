e26a52b05fe1cfa6e35bb11df79024116cdf9c28
Denis Richtarik <denis.richtarik@gmail.com>
2017-12-18 19:10:46 +0100

    Update sample data for Brno branch and prepare car service

--- web/src/main/webapp/app/services/cars_service.js
+++ web/src/main/webapp/app/services/cars_service.js
@@ -13,5 +13,43 @@
                 error(response);
 		    });
     }
+
+    this.deleteSelectedCar = function(id, onSuccess, onError){
+        $http.delete(urlBase.concat("/car/" + id)).then(function(httpResponse){
+            if (httpResponse.status === 200){
+                onSuccess();
+            } else {
+                onError();
+            }
+        }, function(httpResponse){
+            onError();
+        });
+    }
+
+    this.getCars = function (request, onSuccess, onError) {
+        var urlAllReservations = urlBase.concat("/car");
+        var response = {};
+        $http.get(urlAllReservations)
+            .then(function(httpResponse) {
+                response.data = httpResponse;
+                response.data.isSuccess = true;
+                onSuccess(response);
+            }, function(httpResponse) {
+                response.data = httpResponse;
+                response.data.isSuccess = false;
+                error(response);
+            });
+    }
+    this.editSelectedCar = function(id, onSuccess, onError){
+        $http.update(urlBase.concat("/car/" + id)).then(function(httpResponse){
+            if (httpResponse.status === 200){
+                onSuccess();
+            } else {
+                onError();
+            }
+        }, function(httpResponse){
+            onError();
+        });
+    }
 }
 Web.App.service('carsService', ['$http', Web.Services.CarsService]);
