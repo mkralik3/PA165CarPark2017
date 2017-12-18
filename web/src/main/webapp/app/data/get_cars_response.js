3607cf5a76a0880da8ba875eb0008d6ea5c7c0a7
Denis Richtarik <denis.richtarik@gmail.com>
2017-12-18 16:49:54 +0100

    Car frontend implementation

--- web/src/main/webapp/app/data/get_cars_response.js
+++ web/src/main/webapp/app/data/get_cars_response.js
@@ -1,4 +1,4 @@
 ﻿Web.Data.GetCarsResponse = function () {
     this.isSuccess = false;
-    this.data = null; // branches array
+    this.data = null;
 }
--- web/src/main/webapp/app/viewmodels/car_viewmodel.js
+++ web/src/main/webapp/app/viewmodels/car_viewmodel.js
@@ -8,5 +8,5 @@
         result.title = this.name;
         result.source = this;
         return result;
-    }
-}
+    };
+};
